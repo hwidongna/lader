98033c9256fbfe41e90b8b8557e77e96bc53562e
leona <leona@postech.ac.kr>
2013-05-08 22:18:40 +0900

    add an Inside-Out word alignment for each train and test therefore, train-model without gap never find the loss 0 parse

---
+++ example/run-n-times.sh
@@ -0,0 +1,11 @@
+N=$1
+
+for d in $(seq 0 1 7)
+do
+    rm train.$d test.$d
+    for i in $(seq 1 1 $N)
+    do
+        ./train-model.sh $d | tail -n1 >> train.$d
+        ./test-model.sh $d  | tail -n1 >> test.$d
+    done
+done
